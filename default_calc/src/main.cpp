798db6a8e9a3f3c1703c366791755a725a7684e6
Алексей Пащенко <st141278@student.spbu.ru>
2026-04-09 11:36:10 +0300

    necessary modifications have been added

--- default_calc/src/main.cpp
+++ default_calc/src/main.cpp
@@ -1,13 +1,15 @@
-#include "calc.hpp"
+#include "../include/calc.hpp"
 
 #include <iostream>
 #include <string>
+#include <iomanip>
 
 int main()
 {
     double current = 0;
+    bool rad_on = true;
     for (std::string line; std::getline(std::cin, line); ) {
-        current = process_line(current, line);
-        std::cout << current << std::endl;
+        current = process_line(current, rad_on, line);
+        std::cout << std::setprecision(9) << current << std::endl;
     }
 }
